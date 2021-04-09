a5ac5a5d52efff1d36f4fc3c088021c283a19b45
Kyle Lynch <kylelynch104@gmail.com>
2021-04-09 16:48:08 -0500

    getStaticPaths

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -1,4 +1,40 @@
-const ArticleDetails = () => {
+import { createClient } from 'contentful'
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+})
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: 'blogPost',
+  })
+
+  const paths = res.items.map(item => {
+    return {
+      params: { slug: item.fields.slug }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: 'blogPost',
+    'fields.slug': params.slug
+  })
+
+  return {
+    props: { article: items[0] }
+  }
+}
+
+const ArticleDetails = ({ article }) => {
+  console.log(article)
   return <div>Article Details</div>
 }
 
