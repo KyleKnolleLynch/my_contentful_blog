3cb465b72b27ce4bd0a2b2a8b00ae7041b7a929d
Kyle Lynch <kylelynch104@gmail.com>
2021-04-14 15:19:23 -0500

    webpack fs fix

--- next.config.js
+++ next.config.js
@@ -18,4 +18,14 @@ module.exports = {
       limit: 1,
     },
   },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on fs module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      }
+    }
+
+    return config
+  },
 }
