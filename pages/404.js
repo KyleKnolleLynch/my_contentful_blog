e95dd4d14fd9eb51b0d437cd37175995fd742641
Kyle Lynch <kylelynch104@gmail.com>
2021-04-16 12:06:54 -0500

    custom 404

---
+++ pages/404.js
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+const NotFound = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/')
+    }, 4000)
+  }, [])
+
+  return (
+    <div className='not-found'>
+      <h1>404</h1>
+      <h2>Ooops! That page cannot be found</h2>
+      <p>
+        You will be automatically redirected to the{' '}
+        <Link href='/'>Homepage</Link> in 4 seconds
+      </p>
+      <style jsx>{`
+        .not-found {
+          padding: 0 30px;
+        }
+        h1 {
+          font-size: 3em;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default NotFound
